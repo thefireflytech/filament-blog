4aa01217c59807246a7b10589a5f35cf9250df9e
prazwal-bns <prajwalbns15@gmail.com>
2025-08-29 13:37:53 +0545

    Remove dark mode styles from comment textarea in comment.blade.php

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -8,7 +8,7 @@
         <div class="mb-6">
             <label class="mb-2 block text-sm font-semibold" for="author-comment">Comment *</label>
             <textarea name="comment" rows="4" placeholder="Write your message here" type="text" id="author-comment"
-                      class="form-input relative block w-full rounded-md border-0 bg-white px-4 py-4 text-sm text-gray-900 placeholder-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-black disabled:cursor-not-allowed disabled:opacity-75 dark:bg-gray-900 dark:text-white dark:placeholder-gray-500 dark:ring-gray-700"></textarea>
+                      class="form-input relative block w-full rounded-md border-0 bg-white px-4 py-4 text-sm text-gray-900 placeholder-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-black disabled:cursor-not-allowed disabled:opacity-75"></textarea>
             @error('comment')
             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
