09cd4df0dc34cc4739a0383b1afe2ed1731f31a9
Neon17 <neon.neupane.9@gmail.com>
2026-04-14 09:37:51 +0545

    feat: add Open Graph and Twitter Card settings to SeoDetail
    - Add social media SEO migration fields to `seo_details` table.
    - Revamp `SeoDetailForm` with dedicated tabs for General SEO, Open Graph, and Twitter Cards.
    - Clean up `PostForm` by migrating social SEO concerns to the `SeoDetail` schema.
    - Update `SEOMeta` service to generate distinct `<meta>` tags for social platforms.
    - Implement SEO fallback mechanism in `PostController` (Social -> General -> Default Post).
    - Add related Pest unit tests for social SEO generation and setting fallbacks.

--- src/SEOMeta.php
+++ src/SEOMeta.php
@@ -12,6 +12,14 @@ class SEOMeta
     protected $description;
 
     protected $keywords = [];
+    
+    protected $ogTitle;
+    protected $ogDescription;
+    protected $ogImage;
+
+    protected $twitterTitle;
+    protected $twitterDescription;
+    protected $twitterImage;
 
     public function __construct(private Config $config)
     {
@@ -23,6 +31,7 @@ class SEOMeta
         $title = $this->getTitle();
         $description = $this->getDescription();
         $keywords = $this->keywords;
+        
         if ($title) {
             $html[] = "<title>{$title}</title>";
         }
@@ -37,34 +46,95 @@ class SEOMeta
             $keywords = implode(', ', $keywords);
             $html[] = "<meta name=\"keywords\" content=\"{$keywords}\">";
         }
+        
+        $ogTitle = $this->getOgTitle() ?: $title;
+        $ogDescription = $this->getOgDescription() ?: $description;
+        $ogImage = $this->getOgImage();
+        
+        if ($ogTitle) {
+            $html[] = "<meta property='og:title' content='{$ogTitle}'>";
+        }
+        if ($ogDescription) {
+            $html[] = "<meta property='og:description' content='{$ogDescription}'>";
+        }
+        if ($ogImage) {
+            $html[] = "<meta property='og:image' content='{$ogImage}'>";
+        }
+
+        $twitterTitle = $this->getTwitterTitle() ?: $title;
+        $twitterDescription = $this->getTwitterDescription() ?: $description;
+        $twitterImage = $this->getTwitterImage() ?: $ogImage;
+        
+        if ($twitterTitle) {
+            $html[] = "<meta name='twitter:title' content='{$twitterTitle}'>";
+        }
+        if ($twitterDescription) {
+            $html[] = "<meta name='twitter:description' content='{$twitterDescription}'>";
+        }
+        if ($twitterImage) {
+            $html[] = "<meta name='twitter:image' content='{$twitterImage}'>";
+            $html[] = "<meta name='twitter:card' content='summary_large_image'>";
+        }
 
         return implode(PHP_EOL, $html);
     }
 
     public function setTitle($title)
     {
-        // open redirect vulnerability fix
         $title = str_replace(['http-equiv=', 'url='], '', $title);
         $title = strip_tags($title);
         $this->title = $title;
-
         return $this;
     }
 
     public function setDescription($description)
     {
         $this->description = ! $description ? $description : htmlspecialchars($description, ENT_QUOTES, 'UTF-8', false);
-
         return $this;
     }
 
     public function setKeywords(array $keywords)
     {
-        // clean keywords
         $keywords = array_map('strip_tags', $keywords);
-        // store keywords
         $this->keywords = $keywords;
+        return $this;
+    }
+    
+    public function setOgTitle($title)
+    {
+        $title = str_replace(['http-equiv=', 'url='], '', $title);
+        $this->ogTitle = strip_tags($title);
+        return $this;
+    }
+    
+    public function setOgDescription($description)
+    {
+        $this->ogDescription = ! $description ? $description : htmlspecialchars($description, ENT_QUOTES, 'UTF-8', false);
+        return $this;
+    }
+    
+    public function setOgImage($image)
+    {
+        $this->ogImage = strip_tags($image);
+        return $this;
+    }
 
+    public function setTwitterTitle($title)
+    {
+        $title = str_replace(['http-equiv=', 'url='], '', $title);
+        $this->twitterTitle = strip_tags($title);
+        return $this;
+    }
+    
+    public function setTwitterDescription($description)
+    {
+        $this->twitterDescription = ! $description ? $description : htmlspecialchars($description, ENT_QUOTES, 'UTF-8', false);
+        return $this;
+    }
+    
+    public function setTwitterImage($image)
+    {
+        $this->twitterImage = strip_tags($image);
         return $this;
     }
 
@@ -81,6 +151,36 @@ class SEOMeta
     public function getKeywords()
     {
         return $this->keywords ?? $this->getDefaultKeywords();
+    }
+    
+    public function getOgTitle()
+    {
+        return $this->ogTitle;
+    }
+    
+    public function getOgDescription()
+    {
+        return $this->ogDescription;
+    }
+    
+    public function getOgImage()
+    {
+        return $this->ogImage;
+    }
+
+    public function getTwitterTitle()
+    {
+        return $this->twitterTitle;
+    }
+    
+    public function getTwitterDescription()
+    {
+        return $this->twitterDescription;
+    }
+    
+    public function getTwitterImage()
+    {
+        return $this->twitterImage;
     }
 
     public function getDefaultTitle()
